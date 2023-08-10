f3b46545b9a06e207f6783164e3ce11e71e1c439
NotSault <rosiquevictor@gmail.com>
2023-08-10 22:22:53 +0200

    update

--- routes/errors.js
+++ routes/errors.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/404', (req, res) => {
-    res.status(404).send('Page Not Found');
+// Un middleware pour simuler une erreur
+router.use('/simulate-error', (req, res, next) => {
+    throw new Error("Erreur simulée pour démonstration");
 });
 
-router.get('/500', (req, res) => {
-    res.status(500).send('Internal Server Error');
+// Gestion des différentes erreurs HTTP
+
+// 404 Not Found
+router.use((req, res, next) => {
+    const err = new Error('Page non trouvée');
+    err.status = 404;
+    next(err);
+});
+
+// 500 Internal Server Error
+router.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', { 
+        message: err.message, 
+        error: req.app.get('env') === 'development' ? err : {} 
+    });
 });
 
 module.exports = router;
